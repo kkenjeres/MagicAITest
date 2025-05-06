9373f0a42534836a6bc0214cab0aefe690686c1d
kkenjeres <kkenjeres96@gmail.com>
2025-05-06 12:55:12 +0200

    fix build

--- src/data/navigation/footerNavData.ts
+++ src/data/navigation/footerNavData.ts
@@ -1,7 +1,4 @@
-import mail from "../../../public/assets/icons/mail.svg";
-import settings from "../../../public/assets/icons/settings.svg";
-
 export const FooterNavData = [
-  { id: 1, icon: mail, text: "Invite" },
-  { id: 2, icon: settings, text: "Settings" },
+  { id: 1, icon: "/assets/icons/mail.svg", text: "Invite" },
+  { id: 2, icon: "/assets/icons/settings.svg", text: "Settings" },
 ];
--- src/data/navigation/navData.ts
+++ src/data/navigation/navData.ts
@@ -1,8 +1,5 @@
-import folder from "../../../public/assets/icons/folder.png";
-import person from "../../../public/assets/icons/person.svg";
-import shield from "../../../public/assets/icons/shield.svg";
 export const navData = [
-  { id: 2, icon: folder, text: "Library", path: "/library" },
-  { id: 3, icon: shield, text: "Data rooms", path: "/dataRooms" },
-  { id: 4, icon: person, text: "Visitors", path: "/visitors" },
+  { id: 1, icon: "/assets/icons/folder.png", text: "Library", path: "/library" },
+  { id: 2, icon: "/assets/icons/shield.svg", text: "Data rooms", path: "/dataRooms" },
+  { id: 3, icon: "/assets/icons/person.svg", text: "Visitors", path: "/visitors" },
 ];
