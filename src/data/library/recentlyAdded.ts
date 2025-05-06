25881dafa2711f0a7889bbe91ae067040196ade2
kkenjeres <kkenjeres96@gmail.com>
2025-05-06 02:47:06 +0200

    fix

--- src/data/library/recentlyAdded.ts
+++ src/data/library/recentlyAdded.ts
@@ -1,28 +1,23 @@
-import folder from "../../../public/assets/icons/folder.png";
-import avatar from "../../../public/assets/icons/avatar.svg";
-import threeD from "../../../public/assets/icons/3d.svg";
-import mp4 from "../../../public/assets/icons/mp4.svg";
-
 export const recentlyAddedData = [
   {
     id: "ai",
     name: "AI",
-    image: folder,
-    publisher: avatar,
+    image: "/assets/icons/folder.png",
+    publisher: "/assets/icons/avatar.svg",
     date: "May 27, 2025",
   },
   {
     id: "key-account",
     name: "Key Account Plans",
-    image: threeD,
-    publisher: avatar,
+    image: "/assets/icons/3d.svg",
+    publisher: "/assets/icons/avatar.svg",
     date: "May 26, 2025",
   },
   {
     id: "series-b",
     name: "Series B",
-    image: mp4,
-    publisher: avatar,
+    image: "/assets/icons/mp4.svg",
+    publisher: "/assets/icons/avatar.svg",
     date: "May 24, 2025",
   },
 ];
